7de016f58c4bb03a9c0c25736b136f797773b8fe
tomaszwisnia <tomek3646@gmail.com>
2019-07-25 22:20:16 +0200

    display static marker

--- src/app/station/station.component.ts
+++ src/app/station/station.component.ts
@@ -11,13 +11,30 @@ import { latLng, tileLayer, marker, icon } from 'leaflet';
 export class StationComponent implements OnInit {
 
   stationIndex: number;
+  stations: any;
 
   private url = 'http://www.poznan.pl/mim/plan/map_service.html?mtype=pub_transport&co=stacje_rowerowe';
   
   latitude = 52.3992702;
   longitude = 16.9199655;
 
-  summit = marker([ 52.3992702, 16.9199655 ], {
+  constructor(private http: HttpClient, private router: ActivatedRoute) {       
+  }
+  
+  
+  ngOnInit() {
+    this.router.params.subscribe( params =>
+      this.stationIndex = params['id'] 
+    );
+    this.http.get(this.url)
+    .subscribe(data => {
+      this.stations = data;
+      this.latitude = this.stations.features[this.stationIndex].geometry.coordinates[0];
+      this.longitude = this.stations.features[this.stationIndex].geometry.coordinates[1];
+    });
+  }
+  
+  summit = marker([this.latitude, this.longitude], {
     icon: icon({
       iconSize: [ 40, 40 ],
       iconAnchor: [ 13, 41 ],
@@ -25,15 +42,6 @@ export class StationComponent implements OnInit {
     })
   });
 
-  constructor(private http: HttpClient, private router: ActivatedRoute) { 
-  }
-  
-
-  ngOnInit() {
-    this.router.params.subscribe( params =>
-      this.stationIndex = params['id']      
-    );
-  }
   options = {
     layers: [
       tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?key=HfiQgsMsSnorjEs2Sxek', {
@@ -42,6 +50,6 @@ export class StationComponent implements OnInit {
       this.summit
     ],
     zoom: 11,
-    center: latLng([ this.latitude, this.longitude ]),    
+    center: latLng([this.latitude, this.longitude]) 
   };  
 }
